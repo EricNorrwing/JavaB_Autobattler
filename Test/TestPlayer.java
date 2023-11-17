276072218e72802b90f494de99c45d535e5c0668
EricNorrwing <eric.norrwing@gmail.com>
2023-11-17 11:55:35 +0100

    added some player tests

--- Test/TestPlayer.java
+++ Test/TestPlayer.java
@@ -1,30 +1,76 @@
 import com.EricNorrwing.Autobattler.controllers.GameController;
-import com.EricNorrwing.Autobattler.input.InputScanner;
-import com.EricNorrwing.Autobattler.models.Armor;
-import com.EricNorrwing.Autobattler.models.Player;
+import com.EricNorrwing.Autobattler.models.*;
 import com.EricNorrwing.Autobattler.viewers.GameViewer;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import com.EricNorrwing.Autobattler.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
 
 public class TestPlayer {
 
     static GameViewer gv = new GameViewer();
+    static GameController gc = new GameController();
     Player player;
+    Enemy enemy;
+    Armor armor = new Armor();
+    Weapon weapon = new Weapon();
 
     @BeforeEach
     public void setupPlayer(){
         this.player = gv.initializeGame();
+        //Generate a random enemy
+        enemy = gc.generateEnemy(player);
+        armor.generateStartingArmor();
+        weapon.generateStarterWeapon();
     }
     @DisplayName("Testing player class")
     @Test
-    public void testPlayerClass(){
-        Armor armor = new Armor();
-        armor.generateStartingArmor();
+    public void testPlayerClass() {
         armor.setAgilityModifier(5);
+        weapon.setStrengthModifier(5);
         player.getAgility();
-        Assertions.assertEquals(player.getAgility()+armor.getAgilityModifier(), 10);
+        //Test if adding stats through armor gives expected results, starting items have no stats.
+        Assertions.assertEquals(10, player.getAgility()+armor.getAgilityModifier());
+        //Same for weapons
+        Assertions.assertEquals(10, player.getAgility()+armor.getAgilityModifier());
+
+
+        //Testing that player health is within reasonable amounts, above 0 and lower than theoretical max health.
+        //There are scenarios in the game where max health can be above this value, but only by adding weapons and armor
+        Assertions.assertTrue(0 <= player.getHealth() && player.getHealth() <= (int) (100*Math.pow(1.2,player.getLevel())));
+
+        //Make it deal damage to the player, insert player weapons because they dont have stats
+        //Ensure that player health gets reduced by the damage dealt to the player
+        int health = player.getHealth();
+        int damage = enemy.getDamage(weapon, armor);
+        Assertions.assertEquals(health-damage, player.getHealth()-damage);
+        //same but for enemies
+        //TODO Move to enemy class
+        health = enemy.getHealth();
+        damage = player.getDamage(weapon, armor);
+        Assertions.assertEquals(health-damage, enemy.getHealth()-damage);
+
+        //Copy "runFight()" but without sleep
+
+
+    }
+    @DisplayName("Repeated test to ensure damage is always 0 or above")
+    @RepeatedTest(500)
+    public void damageAbove0 (){
+        Assertions.assertTrue(player.getDamage(weapon, armor) >= 0);
+    }
+    //copy of runFight() in gamecontroller that doesnt slow it down.
+    @DisplayName("Test that ensures not both player and enemy dies in a fight")
+    @RepeatedTest(500)
+    public void testFight(){
+        do {
+            if (player.isPlayerTurn()) {
+                player.attack(player, enemy, player.getWeapon(), player.getArmor());
+
+            } else {
+                enemy.attack(enemy, player, enemy.getWeapon(), enemy.getArmor());
+            }
+            player.setPlayerTurn(!player.isPlayerTurn());
+        } while (!AUnit.checkIfDead(player, enemy));
     }
 }
